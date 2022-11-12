99bf0c848b78c9b36354729fe9627d4e76eef3cc
白小飞 <707401057@qq.com>
2022-11-12 20:26:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -221,10 +221,9 @@ def main():
         tbs = get_tbs(i)
         favorites = get_favorite(i)
         for j in favorites:
-            time.sleep(random.randint(1,5))
+            time.sleep(random.randint(1,30))
             client_sign(i, tbs, j["id"], j["name"])
         logger.info("完成第" + str(n) + "个用户签到")
-        sleep(random.randint(0,60))
 #     send_email(favorites)
     logger.info("所有用户签到结束")
 
